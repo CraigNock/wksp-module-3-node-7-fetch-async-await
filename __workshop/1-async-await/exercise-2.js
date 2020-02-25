0e019b20fb62bbcb6200e2f9780627de6f03ff00
Craig Nockels <cnockels@gmail.com>
2020-02-25 12:31:46 -0500

    ex2 done

--- __workshop/1-async-await/exercise-2.js
+++ __workshop/1-async-await/exercise-2.js
@@ -10,7 +10,7 @@
 //         }, 2000);
 //         // return dubs;
 //     } catch (err) {console.log(err)}
-// };
+// }; NO NEED FOR AWAITS, READ THE QUESTION
 
 const doublesLater = (num) => {
     return new Promise(resolve => {
@@ -22,3 +22,36 @@ const doublesLater = (num) => {
 
 doublesLater(2)
     .then((data) => {console.log(data)});
+
+
+    // function addPromise(num){
+    //     return new Promise(resolve => {
+    //         doubleAfter2Seconds(10)
+    //             .then((a) => {
+    //                 doubleAfter2Seconds(20)
+    //                     .then((b) => {
+    //                         doubleAfter2Seconds(30).then((c) => {
+    //                             resolve(num + a + b + c);
+    //                         })
+    //                 })
+    //         })
+    //     });
+    // }
+
+const addPromise = async (num) => {
+    try {
+        let sum = num;
+        sum += await doublesLater(10);
+        sum += await doublesLater(20);
+        sum += await doublesLater(30);
+//this is returning a promise, not just a value. Important maybe for promise interactions.
+        return new Promise(resolve => {resolve(sum)}); 
+    } catch (err) {console.log(err)}
+}
+
+
+
+
+    addPromise(10).then((sum) => {
+        console.log(sum);
+    });
